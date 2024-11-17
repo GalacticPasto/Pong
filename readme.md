96eabd1538080182203a0a931e4ddff221722feb
galacticPast <adhikarybrizein2003@gmail.com>
2024-11-17 13:32:51 -0500

    readme.md

---
+++ readme.md
@@ -0,0 +1,39 @@
+# Pong 
+
+The classic game of pong written in C and implemented with Raylib.
+
+# Platform support
+
+Windows and Linux are currently supported. 
+
+# Prerequisites
+
+- clang or gcc 
+- Visual Studio Community (for windows)
+- Git 
+- Raylib : https://github.com/raysan5/raylib
+
+# Start
+To get started, get all of the prerequisites for your current platform (see above). After this, clone the repository: `git clone https://github.com/GalacticPasto/Pong`.
+
+# Building
+
+Open up a command prompt or Powershell instance to edit the batch or shell script file. Make sure that the includeFlags and the library path has the correct file path to your raylib installation folder. 
+
+## Building: Windows
+Open up Developper Command Prompt for VS 'your version of VS studio code' and navigate to the cloned project directory. After that run the build.bat file.
+
+## Building: Linux
+In the command prompt instance run the build.sh file.
+
+# Running
+The executable should be located in the build folder. Just run the executable and have fun :).
+
+# Keybinds
+- W- player 1 up 
+- S- player 1 down  
+- Arrow up - player 2 up 
+- Arrow down - player 2 down 
+- Space- pause 
+
+
--- src/Pong.c
+++ src/Pong.c
@@ -7,171 +7,169 @@ struct Paddle
 {
     Vector2 position;
     Vector2 size;
-    int score;
+    int     score;
 };
 
-struct Ball 
+struct Ball
 {
     Vector2 position;
     Vector2 velocity;
-    float radius;
+    float   radius;
 };
 
-void ballReset(struct Ball* ball)
+void ballReset(struct Ball *ball)
 {
-    ball->position.x = screenWidth/2.00;
-    ball->position.y = screenHeight/2.00;
-    ball->velocity.x = 7; 
-    ball->velocity.y = 7; 
+    ball->position.x = screenWidth / 2.00;
+    ball->position.y = screenHeight / 2.00;
+    ball->velocity.x = 7;
+    ball->velocity.y = 7;
     ball->radius = 10;
 }
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-
     const int paddleWidth = 20;
     const int paddleHeight = 80;
 
     const int increments = 10;
 
     InitWindow(screenWidth, screenHeight, "Pong");
-    
+
     struct Paddle paddles[2] = {0};
 
     paddles[0].position.x = 0;
     paddles[0].position.y = 0;
     paddles[0].size.x = paddleWidth;
-    paddles[0].size.y = paddleHeight; 
-    paddles[0].score = 0; 
+    paddles[0].size.y = paddleHeight;
+    paddles[0].score = 0;
 
     paddles[1].position.x = screenWidth - paddleWidth;
     paddles[1].position.y = 0;
     paddles[1].size.x = paddleWidth;
     paddles[1].size.y = paddleHeight;
-    paddles[1].score = 0; 
+    paddles[1].score = 0;
 
     struct Ball ball = {0};
-     
+
     ballReset(&ball);
 
     int key = GetKeyPressed();
-     
 
+    SetTargetFPS(60);
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-    
+    bool pause = false;
 
-    // Main game loop
-    while (!WindowShouldClose())    // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        if(!IsKeyDown(key))
-        {
-            key = GetKeyPressed(); 
-        }  
-        switch(key)
-        {
-            case KEY_W:
-                {
-                    if(paddles[0].position.y >= increments)
-                    {
-                        paddles[0].position.y -= increments;
-                    }
-                }break;
-            case KEY_S:
-                {
-                    if(paddles[0].position.y < screenHeight - paddleHeight)
-                    {
-                        paddles[0].position.y += increments;
-                    }
-                }break;
-            case KEY_UP:
-                {
-                    if(paddles[1].position.y >= increments)
-                    {
-                        paddles[1].position.y -= increments;
-                    }
-                }break;
-            case KEY_DOWN:
-                {
-                    if(paddles[1].position.y < screenHeight - paddleHeight)
-                    {
-                        paddles[1].position.y += increments;
-                    }
-                }break;
-        }
-        
-        
-        ball.position.x += ball.velocity.x; 
-        ball.position.y += ball.velocity.y; 
 
-
-        if(ball.position.y + ball.radius >= screenHeight || ball.position.y - ball.radius <= 0)
+        if (IsKeyPressed(KEY_SPACE))
         {
-            ball.velocity.y *= -1;
-        }
-
-        
-        for(int i = 0 ; i < 2 ; i++)
-        {
-            if(ball.position.x >= paddles[i].position.x && ball.position.x <= paddles[i].position.x + paddles[i].size.x
-            && ball.position.y >= paddles[i].position.y && ball.position.y <= paddles[i].position.y + paddles[i].size.y)
+            if (pause)
             {
-                ball.velocity.x *= -1;
+                pause = false;
+            }
+            else
+            {
+                pause = true;
             }
         }
 
-        if(ball.position.x >= screenWidth)
+        if (!IsKeyDown(key))
         {
-            paddles[0].score++;
-            ballReset(&ball);
+            key = GetKeyPressed();
         }
-        if(ball.position.x <= 0)
+        if (pause)
         {
-            paddles[1].score++; 
-            ballReset(&ball);
+
+            switch (key)
+            {
+            case KEY_W: {
+                if (paddles[0].position.y >= increments)
+                {
+                    paddles[0].position.y -= increments;
+                }
+            }
+            break;
+            case KEY_S: {
+                if (paddles[0].position.y < screenHeight - paddleHeight)
+                {
+                    paddles[0].position.y += increments;
+                }
+            }
+            break;
+            case KEY_UP: {
+                if (paddles[1].position.y >= increments)
+                {
+                    paddles[1].position.y -= increments;
+                }
+            }
+            break;
+            case KEY_DOWN: {
+                if (paddles[1].position.y < screenHeight - paddleHeight)
+                {
+                    paddles[1].position.y += increments;
+                }
+            }
+            break;
+            }
+
+            ball.position.x += ball.velocity.x;
+            ball.position.y += ball.velocity.y;
+
+            if (ball.position.y + ball.radius >= screenHeight || ball.position.y - ball.radius <= 0)
+            {
+                ball.velocity.y *= -1;
+            }
+
+            for (int i = 0; i < 2; i++)
+            {
+                if (ball.position.x >= paddles[i].position.x &&
+                    ball.position.x <= paddles[i].position.x + paddles[i].size.x &&
+                    ball.position.y >= paddles[i].position.y &&
+                    ball.position.y <= paddles[i].position.y + paddles[i].size.y)
+                {
+                    ball.velocity.x *= -1;
+                }
+            }
+
+            if (ball.position.x >= screenWidth)
+            {
+                paddles[0].score++;
+                ballReset(&ball);
+            }
+            if (ball.position.x <= 0)
+            {
+                paddles[1].score++;
+                ballReset(&ball);
+            }
         }
-        
 
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-    //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(BLACK);
-            DrawCircleV(ball.position,ball.radius,WHITE);
-            for(int i = 0 ; i < 2 ; i++)
+        ClearBackground(BLACK);
+        DrawCircleV(ball.position, ball.radius, WHITE);
+        for (int i = 0; i < 2; i++)
+        {
+            if (i == 0)
             {
-                if(i == 0)
-                {
-                    DrawText(TextFormat("%d",paddles[i].score), 120, 0, 40,WHITE);       // Draw text (using default font)
-                }
-                else
-                {
-                    DrawText(TextFormat("%d",paddles[i].score), screenWidth - 120, 0, 40, WHITE);       // Draw text (using default font)
-                }
-                DrawRectangleV(paddles[i].position, paddles[i].size,WHITE);                                  
+                DrawText(TextFormat("%d", paddles[i].score), 120, 0, 40, WHITE);
             }
-            
+            else
+            {
+                DrawText(TextFormat("%d", paddles[i].score), screenWidth - 120, 0, 40, WHITE);
+            }
+            DrawRectangleV(paddles[i].position, paddles[i].size, WHITE);
+        }
+
+        if (pause == false)
+        {
+            DrawText(TextFormat("GAME PAUSED"), (screenWidth / 2) - 100, (screenHeight / 2) - 40, 40, WHITE);
+        }
+
         EndDrawing();
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
-
-
-
-
-
